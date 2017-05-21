a784201976d3b8ee344d71a5bac3b23cd5913604
debelopumento <imdiye@gmail.com>
2017-05-21 11:42:37 -0700

    added board render

---
+++ src/Board.js
@@ -0,0 +1,50 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import Square from "./Square";
+import Knight from "./Knight";
+
+export default class Board extends PureComponent {
+	static propTypes = {
+		knightPosition: PropTypes.arrayOf(PropTypes.number.isRequred).isRequred
+	};
+
+	renderSquare(i) {
+		const x = i % 8;
+		const y = Math.floor(i / 8);
+		const black = (x + y) % 2 === 1;
+
+		const [knightX, knightY] = this.props.knightPosition;
+		const piece = x === knightX && y === knightY ? <Knight /> : null;
+		return (
+			<div
+				key={i}
+				style={{
+					width: 50,
+					height: 50
+				}}
+			>
+				<Square black={black}>
+					{piece}
+				</Square>
+			</div>
+		);
+	}
+
+	render() {
+		const squares = [];
+		for (let i = 0; i < 64; i++) {
+			squares.push(this.renderSquare(i));
+		}
+		return (
+			<div
+				style={{
+					width: 400,
+					display: "flex",
+					flexWrap: "wrap"
+				}}
+			>
+				{squares}
+			</div>
+		);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import Knight from "./Knight";
 import Square from "./Square";
+import Board from "./Board";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 
 ReactDOM.render(
-	<Square black>
-		<Knight />
-	</Square>,
+	<Board knightPosition={[0, 0]} />,
 	document.getElementById("root")
 );
 registerServiceWorker();
